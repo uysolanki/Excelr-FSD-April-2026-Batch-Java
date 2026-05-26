4a715eb1a01b310e2cc3a10c61a906c7e62dd85f
uysolanki <uysolanki@gmail.com>
2026-05-26 17:33:01 +0530

    prime function

---
+++ src/day1/ArmstrongNumber4.java
@@ -0,0 +1,33 @@
+package day1;
+
+import java.util.Scanner;
+
+public class ArmstrongNumber4 {
+
+	public static void main(String[] args) {
+		for(int n1=1;n1<=1000;n1++)
+		{
+		if (checkArmstrong(n1)) 
+			System.out.println(n1 + " number is Armstrong");
+		}
+	}
+
+	private static boolean checkArmstrong(int n1) {
+		int bu = n1;
+		int sum = 0;
+		int rem = 0;
+		while (n1 > 0) 
+		{ 
+			rem = n1 % 10; 
+			sum = sum + (rem * rem * rem); 
+			n1 = n1 / 10; 
+		}
+		if (sum == bu) {
+			return true;
+		} else {
+			return false;
+		}
+
+	}
+
+}
