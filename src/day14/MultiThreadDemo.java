2e8794c7ce174953a418415bc37f44121f00bc9b
uysolanki <uysolanki@gmail.com>
2026-06-11 19:12:21 +0530

    Multi threading part 1

--- src/day14/MultiThreadDemo.java
+++ src/day14/MultiThreadDemo.java
@@ -3,9 +3,15 @@ package day14;
 public class MultiThreadDemo {
 
 	public static void main(String[] args) {
+		System.out.println("Priority of main "+Thread.currentThread().getPriority()); 
 		Alpha thread1=new Alpha("Alpha");
 		Beta thread2=new Beta("Bravo");
 		Gama thread3=new Gama("Charlie");
+		
+		thread1.setPriority(1);
+		thread2.setPriority(5);
+		thread3.setPriority(10);
+		//thread1.setPriority(Thread.MIN_PRIORITY);
 		
 		thread1.start();	//Thread-0
 		thread2.start();	//Thread-1
@@ -18,3 +24,7 @@ public class MultiThreadDemo {
 	}
 
 }
+
+//Min 1
+//Norm 5   - main()  thread inherits the priority of its parent  default priority
+//Max 10
