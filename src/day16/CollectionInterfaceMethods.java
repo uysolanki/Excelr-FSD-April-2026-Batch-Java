81f4082868757e13d558b99b259e761ec0e1dcb0
uysolanki <uysolanki@gmail.com>
2026-06-15 18:50:06 +0530

    Collection part 1

--- src/day16/CollectionInterfaceMethods.java
+++ src/day16/CollectionInterfaceMethods.java
@@ -31,6 +31,14 @@ public class CollectionInterfaceMethods {
 		System.out.println(names.size()); //0
 		System.out.println(names.isEmpty());  //true
 		
+		
+		ArrayList<Integer> numbers=new ArrayList();
+		numbers.add(1);
+		numbers.add(2);
+		numbers.add(3);
+		
+		numbers.remove(3);					//integer index
+		System.out.println(numbers);
 	}
 
 }
