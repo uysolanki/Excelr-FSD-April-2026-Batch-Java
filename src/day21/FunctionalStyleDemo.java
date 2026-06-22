97eea3d050e099bafcbb438085e76b0235c9a34c
uysolanki <uysolanki@gmail.com>
2026-06-22 19:29:27 +0530

    stream api

--- src/day21/FunctionalStyleDemo.java
+++ src/day21/FunctionalStyleDemo.java
@@ -3,6 +3,8 @@ package day21;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Function;
+import java.util.function.Predicate;
 
 public class FunctionalStyleDemo {
 
@@ -34,7 +36,7 @@ public class FunctionalStyleDemo {
 		.map(per->per+5.0)
 		.forEach(per->System.out.println(per));
 		
-		
+													// [ 8 , 7,5,5]  String->Integer
 		List<String> months=new ArrayList(Arrays.asList("January","February","March","April","May","June","July","August","September","October","November","December"));
 		//1. display month names which has >= 5 chars
 		System.out.println("********************");
@@ -44,7 +46,58 @@ public class FunctionalStyleDemo {
 		
 		System.out.println("********************");
 		months.stream()
-		.filter(FunctionalStyleDemo::checkMonthLength)
+		.filter(FunctionalStyleDemo::checkMonthLength)  //by using method referncing
+		.forEach(System.out::println);
+		
+		
+		//multiply each number by 5
+		//extract the odd numbers
+		//find their sum
+		
+		
+		//sum of all elements		[1,2,3,4,5]		acc=0,1,3,6,10,15
+		int sum=numbers.stream()
+		.reduce(0,(acc,num)->acc+num);
+		
+		System.out.println(sum);
+		
+		
+		
+		int sum1=numbers.stream()	//[1,2,3,4,5]	
+		.map(num->num*5)	//[5,10,15,20,25]	
+		.filter(num->num%2==1)	//[5,15,25]	
+		.reduce(0,(acc,num)->acc+num);
+		
+		System.out.println(sum1);
+		
+		Predicate<Integer> oddFilter=num->num%2==1;
+		int sum2=numbers.stream()	//[1,2,3,4,5]	
+				.map(num->num*5)	//[5,10,15,20,25]	
+				.filter(oddFilter)	//[5,15,25]				//by using Predicate
+				.reduce(0,(acc,num)->acc+num);
+				
+				System.out.println(sum1);
+			
+		
+				
+		List<Integer> monthsLengths=months.stream()
+		.map(month->month.length())
+		.toList();
+		System.out.println(monthsLengths);
+		
+		
+		Function<String,Integer> function1=str->str.length();
+		List<Integer> monthsLengths1=months.stream()
+				.map(function1)
+				.toList();
+				System.out.println(monthsLengths1);
+				
+		months.stream()
+		.map(month->month.toUpperCase())
+		.forEach(month->System.out.println(month));
+		
+		months.stream()
+		.map(String::toUpperCase)
 		.forEach(System.out::println);
 	}
 	
